646aa0bb1f801e28db3b23161c5b8523d13f9f47
leminhduc1512 <82688100+leminhduc1512@users.noreply.github.com>
2022-03-07 20:17:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'hellu Trà!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao Trà cứ đi nhầm đường
+    Lạc vào tim Đức lẻ loi.
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'haha',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích <33',
+    btnNo: 'Hông -_-',
+    question:'Có gì nói hông nè',
+    btnReply: 'Gửi',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'iuthuong',
+    btnAccept: 'Okiiiii nè <3',
+    messLink: 'https://leminhduc15122000.github.io/' 
+}
